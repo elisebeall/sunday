734267970416d6689d46dfb66afa9096d673756a
Elise Beall <elisebeall@gmail.com>
2022-03-02 01:20:41 -0700

    Add display of sections to main page

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,12 +1,18 @@
 import { Fragment } from 'react';
+import { Link } from 'react-router-dom';
 import Articles from './Articles';
-//import { v4 as uuid } from 'uuid';
+import { v4 as uuid } from 'uuid';
+import sections from '../assets/sections.json';
 import '../css/Main.css';
 
 const Main = () => {
+  const displaySections = sections.map(section => {
+    return <Link to={`/${section}`} key={uuid()}><div>{section}</div></Link>
+  })
+
   return (
     <>
-      <Articles />
+      {displaySections}
     </>
   )
 }
