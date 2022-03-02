8cd7528d9a807c89b597b2396a620e007fd6a981
Elise Beall <elisebeall@gmail.com>
2022-03-02 01:19:39 -0700

    Implement useParams hook to display articles based on section

--- src/components/Articles.js
+++ src/components/Articles.js
@@ -1,4 +1,5 @@
 import { Fragment } from 'react';
+import { useParams } from 'react-router-dom';
 import { v4 as uuid } from 'uuid';
 import Error from './Error';
 import Loading from './Loading';
@@ -8,7 +9,8 @@ import endpoints from '../endpoints.js';
 import '../css/Articles.css';
 
 const Articles = () => {
-  const { data, isLoading, error } = useFetch(endpoints.home);
+  const section = useParams().section;
+  const { data, isLoading, error } = useFetch(endpoints[`${section}`]);
   let articles, displayArticlesList;
 
   if (!isLoading) {
@@ -16,7 +18,7 @@ const Articles = () => {
 
     displayArticlesList = articles.map(article => {
       return <ArticleCard article={article} key={uuid()} />
-    })
+    });
   }
 
   return (
