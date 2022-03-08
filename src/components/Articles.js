494e2c636dbff15efad38dcc5f3457ce705342d2
Elise Beall <elisebeall@gmail.com>
2022-03-08 12:01:51 -0700

    Move fetch call inside Articles, remove hook

--- src/components/Articles.js
+++ src/components/Articles.js
@@ -1,24 +1,52 @@
-import { Fragment } from 'react';
+import { Fragment, useContext, useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
+import { ArticlesContext } from '../contexts/ArticlesContext';
 import { v4 as uuid } from 'uuid';
+import NotFound from './NotFound';
 import Error from './Error';
 import Loading from './Loading';
 import ArticleCard from './ArticleCard';
-import useFetch from '../hooks/useFetch';
+import usePath from '../hooks/usePath';
 import endpoints from '../endpoints.js';
+import sectionsList from '../assets/sections.json';
 import '../css/Articles.css';
 
 const Articles = () => {
-  let { section } = useParams();
-  if (!section) {
-    section = 'home';
-  }
-  const { data, isLoading, error } = useFetch(endpoints[`${section}`]);
-  let articles, displayArticlesList;
+  let path = useParams().section;
+  const { section, pathError } = usePath(path);
+  const [articles, setArticles] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
+  let displayArticlesList;
+
+  useEffect(() => {
+    const getData = async () => {
+      const response = await fetch(endpoints[`${section}`]);
+
+      if (response.status !== 200) {
+        throw new Error(`${response.status}: ${response.statusText}. Cannot fetch articles.`);
+      } else if (!response.ok) {
+        throw new Error('Something has gone wrong, please try again.')
+      }
+
+      const data = await response.json();
+      return data;
+    }
+
+    getData()
+      .then(data => {
+        setArticles(data.results);
+        setError(null);
+        setIsLoading(false);
+      })
+      .catch(error => {
+        setError(error.message);
+        setArticles(null);
+        setIsLoading(false);
+      })
+  }, [section])
 
   if (!isLoading) {
-    articles = data?.results;
-
     displayArticlesList = articles.map(article => {
       return <ArticleCard article={article} key={uuid()} />
     });
@@ -26,11 +54,15 @@ const Articles = () => {
 
   return (
     <div className="articles-container">
-      {isLoading ? <Loading /> :
+      {pathError ? <NotFound /> :
         <>
-          {error ? <Error error={error} /> :
+          {isLoading ? <Loading /> :
             <>
-              {displayArticlesList}
+              {error ? <Error error={error} /> :
+                <>
+                  {displayArticlesList}
+                </>
+              }
             </>
           }
         </>
